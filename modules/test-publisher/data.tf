211fa3fafd9422182844e4976a8b792c34e221ee
EliMeitner <eli.meitner@toluna.com>
2022-02-16 06:24:47 +0200

    Update data.tf

--- modules/test-publisher/data.tf
+++ modules/test-publisher/data.tf
@@ -15,23 +15,10 @@ data "aws_iam_policy_document" "codebuild_assume_role_policy" {
 data "aws_iam_policy_document" "codebuild_role_policy" {
   statement {
     actions   = [
-          "s3:*",
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-          "ssm:*",
-          "codebuild:*"
-        ]
-    resources = [
-          "${data.aws_s3_bucket.codepipeline_bucket.arn}",
-          "${data.aws_s3_bucket.codepipeline_bucket.arn}/*"
-        ]
-  }
-  statement {
-    actions   = [
           "logs:*",
           "ssm:*",
-          "s3:*"
+          "s3:*",
+          "codebuild:*"
         ]
     resources = ["*"]
   }
