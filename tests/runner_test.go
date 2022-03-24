6cca415d5f3c9dfcb547a1874d8dcc8b756ebe3c
EliMeitner <eli.meitner@toluna.com>
2022-03-24 08:34:13 +0200

    Update runner_test.go

--- tests/runner_test.go
+++ tests/runner_test.go
@@ -7,7 +7,6 @@ import (
 	"github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
-	test_structure "github.com/gruntwork-io/terratest/modules/test-structure"
 	"github.com/stretchr/testify/assert"
 )
 
@@ -40,40 +39,33 @@ func configureTerraformOptions(t *testing.T) *terraform.Options {
 }
 
 // An example of how to test the Terraform module in examples/terraform-aws-ecs-example using Terratest.
-func TestSetup(t *testing.T) {
+func TestTerraformTestFramework(t *testing.T) {
 	t.Parallel()
-	t.Name()
-	// Pick a random AWS region to test in. This helps ensure your code works in all regions.
-	// Construct the terraform options with default retryable errors to handle the most common retryable errors in
-	// terraform testing.
-
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
-	defer test_structure.RunTestStage(t, "destroy", func() {
-		terraformOptions := configureTerraformOptions(t)
-		terraform.Destroy(t, terraformOptions)
-	})
-
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	test_structure.RunTestStage(t, "setup", func() {
-		terraformOptions := configureTerraformOptions(t)
-		terraform.InitAndApply(t, terraformOptions)
-	})
-
-	test_structure.RunTestStage(t, "validate", func() {
-		terraformOptions := configureTerraformOptions(t)
-		testBucketExists(t, terraformOptions)
-	})
-	test_structure.RunTestStage(t, "validate", func() {
-		terraformOptions := configureTerraformOptions(t)
-		testAssertFail(t, terraformOptions)
-	})
+	for _, testFuncs := range []struct {
+		name  string
+		tfunc func(*testing.T)
+	}{
+		{"Terraform Init", testSetup},
+		{"Bucket Exists", testBucketExists},
+		{"Test AssertFail", testAssertFail},
+		{"Clean up", testCleanUp}} {
+		t.Run(testFuncs.name, testFuncs.tfunc)
+	}
 
 }
 
-func testBucketExists(t *testing.T, terraformOptions *terraform.Options) {
+func testSetup(t *testing.T) {
+	terraform.InitAndApply(t, configureTerraformOptions(t))
+}
+
+func testCleanUp(t *testing.T) {
+	terraform.Destroy(t, configureTerraformOptions(t))
+}
+
+func testBucketExists(t *testing.T) {
 	aws.AssertS3BucketExistsE(t, "us-east-1", "test-poc-postman-tests")
 }
 
-func testAssertFail(t *testing.T, terraformOptions *terraform.Options) {
+func testAssertFail(t *testing.T) {
 	assert.Equal(t, "1", "2")
 }
