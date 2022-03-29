9675a3e451c8bfb4fb435bf007d5db100e580162
EliMeitner <eli.meitner@toluna.com>
2022-03-29 12:27:44 +0300

    Update main.tf, cover.go, and runner_test.go

--- tests/runner_test.go
+++ tests/runner_test.go
@@ -10,7 +10,7 @@ import (
 	"github.com/gruntwork-io/terratest/modules/random"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
-	tools "github.com/toluna-terraform/terraform-test-library/modules/coverage"
+	"github.com/toluna-terraform/terraform-test-library/modules/coverage"
 	"golang.org/x/mod/modfile"
 )
 
@@ -57,18 +57,18 @@ var moduleName = getModName()
 func TestSetup(t *testing.T) {
 	terraform.InitAndApply(t, configureTerraformOptions(t))
 	log.Println("Running Terraform init")
-	tools.WriteConvergeFiles(t, configureTerraformOptions(t), moduleName)
+	coverage.WriteConvergeFiles(t, configureTerraformOptions(t), moduleName)
 }
 
 func TestBucketExists(t *testing.T) {
 	log.Println("Checking for test bucket")
-	tools.MarkAsCovered("aws_s3_bucket_policy.postman_bucket", moduleName)
+	coverage.MarkAsCovered("aws_s3_bucket_policy.postman_bucket", moduleName)
 	err := aws.AssertS3BucketExistsE(t, "us-east-1", "test-poc-postman-tests")
 	assert.Nil(t, err, "Bucket not found")
 }
 func TestTerraformTestLambda(t *testing.T) {
 	log.Println("invoking test-runner Lambda")
-	tools.MarkAsCovered("aws_lambda_function.test_framework", moduleName)
+	coverage.MarkAsCovered("aws_lambda_function.test_framework", moduleName)
 	var invocationType aws.InvocationTypeOption = aws.InvocationTypeRequestResponse
 	input := &aws.LambdaOptions{
 		InvocationType: &invocationType,
